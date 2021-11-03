1785e2d12596d554e13fc39597c31e36d45e8640
Mhashimea <hashimea12@gmail.com>
2021-11-03 11:18:16 +0400

    completed

--- server/options.ts
+++ server/options.ts
@@ -1,10 +1,11 @@
 import { Sequelize } from "sequelize-typescript"
 
 export const sequelize = new Sequelize({
-  username: "root",
-  password: "",
-  database: "restaurant",
-  host: "localhost",
+  username: "doadmin",
+  password: "Xs81MjE8Wi2xcGzh",
+  database: "defaultdb",
+  host: "db-mysql-blr1-17476-do-user-6932806-0.b.db.ondigitalocean.com",
+  port: 25060,
   dialect: "mysql",
   models: [__dirname + "/model"],
 })
